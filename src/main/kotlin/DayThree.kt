f6541103497fcd45f5718e3e2bcef528a37219bf
MEINPC\mukha <mukhamed.ct@gmail.com>
2023-12-03 07:37:51 +0100

    Day 3

---
+++ src/main/kotlin/DayThree.kt
@@ -0,0 +1,83 @@
+fun main() {
+    val engine = getLinesFromFile("day_three.txt").map(String::toList)
+    partOne(engine)
+    partTwo(engine)
+}
+
+private fun Char.isSymbol(): Boolean = this != '.' && !this.isDigit()
+
+val directions = listOf(-1 to 0, 1 to 0, 0 to -1, 0 to 1, -1 to -1, -1 to 1, 1 to -1, 1 to 1)
+
+private fun partOne(engine: List<List<Char>>) {
+    var result = 0
+    for (i in engine.indices) {
+        var j = 0
+        while (j < engine[i].size - 1) {
+            val numberBuilder = StringBuilder()
+            if (!engine[i][j].isDigit()) {
+                j++
+                continue
+            }
+            val adjacencyList = ArrayList<Boolean>()
+            while (j < engine[i].size && engine[i][j].isDigit()) {
+                directions.forEach { (dirI, dirJ) ->
+                    if (i + dirI >= 0 && i + dirI <= engine.size - 1
+                        && j + dirJ >= 0 && j + dirJ <= engine[i].size - 1) {
+                        adjacencyList.add(engine[i + dirI][j + dirJ].isSymbol())
+                    }
+                }
+                numberBuilder.append(engine[i][j])
+                j++
+            }
+            if (adjacencyList.any { it }) {
+                result += numberBuilder.toString().toInt()
+            }
+            j++
+        }
+    }
+    println(result)
+}
+
+private fun partTwo(engine: List<List<Char>>) {
+    var result = 0
+    for (i in engine.indices) {
+        var j = 0
+        while (j < engine[i].size) {
+            if (engine[i][j] != '*') {
+                j++
+                continue
+            }
+            val adjacentNumbers = HashSet<Int>()
+            directions.forEach { (dirI, dirJ) ->
+                if (i + dirI >= 0 && i + dirI <= engine.size - 1
+                    && j + dirJ >= 0 && j + dirJ <= engine[i].size - 1
+                    && engine[i + dirI][j + dirJ].isDigit()) {
+                    adjacentNumbers.add(extractNumberForPosition(engine,i + dirI, j + dirJ))
+                }
+            }
+            if (adjacentNumbers.size == 2) {
+                result += adjacentNumbers.reduce { acc, number -> acc * number }
+            }
+            j++
+        }
+    }
+    println(result)
+}
+
+private fun extractNumberForPosition(
+    engine: List<List<Char>>,
+    i: Int,
+    j: Int
+): Int {
+    val numberBuilder = StringBuilder()
+    var startJ = j
+    while (startJ >= 0 && engine[i][startJ].isDigit()) {
+        startJ--
+    }
+    startJ++
+    while (startJ < engine[i].size && engine[i][startJ].isDigit()) {
+        numberBuilder.append(engine[i][startJ])
+        startJ++
+    }
+    return numberBuilder.toString().toInt()
+}
